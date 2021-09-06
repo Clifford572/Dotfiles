532024f5a96d6fe3764e9620abdea679bab61b43
Clifford572 <65845511+Clifford572@users.noreply.github.com>
2021-09-06 10:49:31 +0000

    Delete nvim directory

--- nvim/init.vim
+++
@@ -1,7 +0,0 @@
-source $HOME/.config/nvim/lua/keymaps.lua
-source $HOME/.config/nvim/lua/settings.lua
-source $HOME/.config/nvim/lua/plugins.lua
-source $HOME/.config/nvim/lua/telescope-config.lua
-source $HOME/.config/nvim/vimscript/coc.vim
-source $HOME/.config/nvim/vimscript/compile.vim
-source $HOME/.config/nvim/vimscript/airline.vim
--- nvim/lua/keymaps.lua
+++
@@ -1,9 +0,0 @@
-vim.g.mapleader = " "
-vim.api.nvim_set_keymap('i','<C-H>','<C-W>',{silent = true})
-vim.api.nvim_set_keymap('n','<C-h>','<C-w>h',{noremap = true, silent = true})
-vim.api.nvim_set_keymap('n','<C-j>','<C-w>j',{noremap = true, silent = true})
-vim.api.nvim_set_keymap('n','<C-k>','<C-w>k',{noremap = true, silent = true})
-vim.api.nvim_set_keymap('n','<C-l>','<C-w>l',{noremap = true, silent = true})
-vim.api.nvim_set_keymap('n','<C-d>','<C-w>q',{noremap = true, silent = true})
-vim.api.nvim_set_keymap('n','<leader>l',':bnext<CR>',{noremap = true, silent = true})
-vim.api.nvim_set_keymap('n','<leader>h',':bprevious<CR>',{noremap = true, silent = true})
